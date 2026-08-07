79e4c11795f1607bfcf4fa1639a99bdd2da5a1c2
Travis <travis@lenarys.com>
2026-08-07 10:21:19 -0400

    Define Marketplace release compatibility

--- version.php
+++ version.php
@@ -25,7 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_la';
-$plugin->version   = 2026080600;
+$plugin->version   = 2026080704;
 $plugin->requires  = 2025041400; // Moodle 5.0 minimum (MOODLE_500_STABLE).
+$plugin->supported = [500, 502];
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.2';
